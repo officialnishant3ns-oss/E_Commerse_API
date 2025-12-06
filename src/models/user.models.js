1b2ac8224e930e7c9be8c0017675eb92f3ce7df3
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-06 12:01:32 +0530

    added user modells

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,1 +1,47 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+        cartdata: {
+            items: {
+                type: Array,
+                default: []
+            },
+            totalQuantity: {
+                type: Number,
+                default: 0
+            },
+            totalPrice: {
+                type: Number,
+                default: 0
+            }
+        }
+
+    },
+    { timestamps: true }
+)
+
+
+
+
+const User = mongoose.model("User", userSchema);
+export default User
