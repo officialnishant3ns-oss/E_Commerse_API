9fb16e8fde4e9c74039390e3d7976370ef3305e3
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-06 13:08:31 +0530

    added password hash in user models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -44,7 +44,15 @@ const userSchema = new mongoose.Schema(
 )
 
 
+userschema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password =await bcrypt.hash(this.password, 10)
+    next()
+})
 
+userschema.methods.isPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 export default User
