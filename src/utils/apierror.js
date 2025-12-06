725d3a5bc0c9aac186a2d0d932a3c4b0b2981b32
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-06 12:14:13 +0530

    added apierror

---
+++ src/utils/apierror.js
@@ -0,0 +1,11 @@
+class ApiError extends Error {
+    constructor(statuscode, message = "Something went wrong", error = [], stack = "") {
+        super(message)
+        this.statuscode = statuscode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.error = error
+    }
+}
+export default ApiError
