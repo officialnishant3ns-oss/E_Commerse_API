d843cd6d38e2411b97c928841a63a04039990182
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-07 18:51:39 +0530

    added jwt generator in function in user models

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,8 @@
 import ApiError from "../utils/apierror.js"
 import ApiResponse from "../utils/apiresponse.js"
 import AsyncHandler from "../utils/asynchandler.js"
-
 import User from "../models/user.models.js"
+import emailValidator from "email-validator"
 
 const register = AsyncHandler(async (req, res) => {
     const { username, email, password } = req.body
@@ -13,13 +13,19 @@ const register = AsyncHandler(async (req, res) => {
     if (userExist) {
         throw new ApiError(409, "User Already Exist Pls Go to Login")
     }
+    if (!emailValidator.validate(email)) {
+        throw new ApiError(400, "Please enter a valid email address")
+    }
+    if (password.length < 8) {
+        throw new ApiError(400, "Password must be at least 8 characters long")
+    }
     const user = await User.create({
         email,
         password,
         username: username.toLowerCase()
     })
     const createduser = await User.findById(user._id).select(
-        "-password -refreshtoken"
+        "-password -refreshtoken -createdAt -updatedAt -__v"
     )
     if (!createduser) {
         throw new ApiError(500, "Something went wrong while registering the user")
@@ -29,5 +35,24 @@ const register = AsyncHandler(async (req, res) => {
         new ApiResponse(200, createduser, "User registered successfully")
     )
 })
+const login = AsyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new ApiError(400, "Please enter required fields")
+    }
+    const userExist = await User.findOne({ email })
+    if (!userExist) {
+        throw new ApiError(404, "User not found, please register")
+    }
+    const isPasswordmatch = await userExist.isPassword(password)
+    if (!isPasswordmatch) {
+        throw new ApiError(401, "Invalid credentials, please try again")
+    }
 
-export { register }
+    return res.status(200).json(
+        new ApiResponse(200, userExist, "User logged in successfully")
+    )
+})
+
+
+export { register, login }
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -29,8 +29,38 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 )
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return
+    this.password = await bcrypt.hash(this.password, 10)
+})
 
-
+userSchema.methods.isPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
 export default User
