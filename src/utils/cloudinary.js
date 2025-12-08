cfccf433b9bd97714122782a1469e0b6d0c8fb08
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-08 12:27:37 +0530

    added cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+const uploadoncloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            console.log(" File path not provided")
+            return null
+        }
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+            folder: 'uploads'
+        })
+
+        console.log("file is uploaded on clodinary", response.secure_url)
+        if (fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath)
+        }
+        return response
+
+    } catch (error) {
+        console.error(" Error:", error);
+        if (fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath)
+        }
+        return null
+    }
+}
+
+
+export default uploadoncloudinary
